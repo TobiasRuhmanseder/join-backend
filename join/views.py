bf0a60a95e52422ccb84f252ba5af7d6945fba5b
Tobias Ruhmanseder <tobi.r1707@gmail.com>
2024-05-30 09:03:37 +0200

    delete any attempt code

--- join/views.py
+++ join/views.py
@@ -8,8 +8,8 @@ from join.serializers import BoardSerializer
 from join.serializers import TaskSerializer
 from join.permissions import IsBoardUser
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.permissions import AllowAny
+
 
 """
     Handles user authentication and token generation.
@@ -23,10 +23,7 @@ class LoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
                                             context={'request': request})
-        try:
-            serializer.is_valid(raise_exception=True)
-        except AuthenticationFailed:
-            return Response({'non_field_errors': ['Unable to log in with provided credentials.']}, status=status.HTTP_401_UNAUTHORIZED)
+        serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
         return Response({
